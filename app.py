0bcad9e18cfe54f82efa9cf303204bc5596cb0e7
Mukesh <mukeshaitha81@gmail.com>
2026-03-29 11:53:06 +0530

    UI FOR WEB

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-# app.py
-
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# ✅ Import your Gemini agent
 from personal_assistant.agent import run_agent
 
 
 @app.route('/')
 def home():
-    return "Hack AI is running 🚀"
+    return render_template("index.html")
 
 
 @app.route('/chat', methods=['POST'])
@@ -23,7 +20,6 @@ def chat():
 
         user_input = data["message"]
 
-        # 🔥 Call your AI agent
         response = run_agent(user_input)
 
         return jsonify({
