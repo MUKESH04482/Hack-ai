a09a715367731c51f489492ce845a6603d16d1f0
Mukesh <mukeshaitha81@gmail.com>
2026-03-29 10:35:14 +0530

    ready for deploy

--- app.py
+++ app.py
@@ -6,7 +6,5 @@ app = Flask(__name__)
 def home():
     return "Hack AI is running 🚀"
 
-@app.route('/api')
-def api():
-    return {"status": "WORKING 🚀"}
-
+if __name__ == "__main__":
+    app.run(debug=True)
